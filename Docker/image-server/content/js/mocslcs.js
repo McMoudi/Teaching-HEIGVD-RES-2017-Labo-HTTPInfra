3ff24bc2ba7ac3d96e082dae814d9056d2250390
mcmoudi <mcmoudi@gmail.com>
2017-06-11 13:54:20 +0200

    adaptation for addresses

--- Docker/image-server/content/js/mocslcs.js
+++ Docker/image-server/content/js/mocslcs.js
@@ -7,7 +7,7 @@ $(function () {
             console.log(data)
             var msg = "Ola, I'm empty!"
             if (data.length > 0) {
-                msg = data[0];
+                msg = data[0].country +", "+ data[0].city + ", "+ data[0].number + " "+data[0].street;
             }
             $(".intro-text").text(msg);
         });
@@ -15,5 +15,5 @@ $(function () {
     }
 
     loadMyJSON();
-
+    setInterval(loadMyJSON, 5000);
 });
